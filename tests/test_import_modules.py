5ba0eadc24e297660f2257c584606643b9c69ef1
Isaac C. F. Wong <290-cfwong@noreply.gitlab.et-gw.edu>
2025-07-22 14:44:57 +0200

    test: Add a test for importing modules

---
+++ tests/test_import_modules.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+import importlib
+
+import pytest
+
+modules_to_test = [
+    "gwsim",
+    "gwsim.generator",
+    "gwsim.generator.base",
+    "gwsim.utils",
+    "gwsim.utils.log",
+    "gwsim.utils.random",
+]
+
+
+@pytest.mark.parametrize("module_name", modules_to_test)
+def test_module_imports(module_name):
+    try:
+        importlib.import_module(module_name)
+    except ImportError as e:
+        pytest.fail(f"Failed to import {module_name}: {e}")
