b72111971df2c2bad98c97a03e7da4e5414ee0ca
Isaac C. F. Wong <chunfung.wong@kuleuven.be>
2025-10-08 16:44:42 +0200

    fix: Fix the metadata

--- src/gwsim/simulator/mixin/randomness.py
+++ src/gwsim/simulator/mixin/randomness.py
@@ -7,7 +7,7 @@ import logging
 from gwsim.simulator.state import StateAttribute
 from gwsim.utils.random import Generator, get_rng, get_state, set_seed, set_state
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("gwsim")
 
 
 class RandomnessMixin:
@@ -103,6 +103,5 @@ class RandomnessMixin:
         Returns:
             Dictionary containing seed and other metadata.
         """
-        metadata = super().metadata if hasattr(super(), "metadata") else {}
-        metadata.update({"seed": self.seed})
+        metadata = {"seed": self.seed}
         return metadata
