9bc7e36296ca3706be4a14963f930d94a3271385
Isaac C. F. Wong <chunfung.wong@kuleuven.be>
2025-10-24 08:32:24 +0200

    feat: Add a detector mixin

---
+++ src/gwsim/simulator/mixin/detector.py
@@ -0,0 +1,28 @@
+"""Detector mixin for simulators."""
+
+from __future__ import annotations
+
+
+class DetectorMixin:  # pylint: disable=too-few-public-methods
+    """Mixin class to add detector information to simulators."""
+
+    def __init__(self, detectors: list[str] | None = None, **kwargs):  # pylint: disable=unused-argument
+        """Initialize the DetectorMixin.
+
+        Args:
+            detectors (list[str] | None): List of detector names. If None, use all available detectors.
+            **kwargs: Additional arguments.
+        """
+        self.detectors = detectors
+
+    @property
+    def metadata(self) -> dict:
+        """Get metadata including detector information.
+
+        Returns:
+            Dictionary containing the list of detectors.
+        """
+        metadata = {
+            "detectors": self.detectors,
+        }
+        return metadata
