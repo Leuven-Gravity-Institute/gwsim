bd63b0f5d0bf4bc6b34ff08d1220d8282bb42dda
Isaac C. F. Wong <chunfung.wong@kuleuven.be>
2025-11-10 04:58:41 +0100

    chore: Add TimeSeriesList to the sub-module import

--- src/gwsim/data/time_series/__init__.py
+++ src/gwsim/data/time_series/__init__.py
@@ -3,5 +3,6 @@
 from __future__ import annotations
 
 from gwsim.data.time_series.time_series import TimeSeries
+from gwsim.data.time_series.time_series_list import TimeSeriesList
 
-__all__ = ["TimeSeries"]
+__all__ = ["TimeSeries", "TimeSeriesList"]
