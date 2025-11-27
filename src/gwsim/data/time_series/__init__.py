2104dd8b74d1d8bd09f32286588f86068718e2ac
Isaac C. F. Wong <chunfung.wong@kuleuven.be>
2025-11-28 07:35:01 +0800

    chore: Expose the inject module

--- src/gwsim/data/time_series/__init__.py
+++ src/gwsim/data/time_series/__init__.py
@@ -2,7 +2,8 @@
 
 from __future__ import annotations
 
+from gwsim.data.time_series import inject
 from gwsim.data.time_series.time_series import TimeSeries
 from gwsim.data.time_series.time_series_list import TimeSeriesList
 
-__all__ = ["TimeSeries", "TimeSeriesList"]
+__all__ = ["TimeSeries", "TimeSeriesList", "inject"]
