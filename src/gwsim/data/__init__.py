b84a982810e3b1c88c2bdf019cf36fe30e7e5e15
Isaac C. F. Wong <chunfung.wong@kuleuven.be>
2025-12-02 12:32:57 +0100

    chore: Remove imports

--- src/gwsim/data/__init__.py
+++ src/gwsim/data/__init__.py
@@ -1,7 +1,3 @@
 """Initialization code for the gwsim.data package."""
 
 from __future__ import annotations
-
-from gwsim.data.time_series import TimeSeries, TimeSeriesList
-
-__all__ = ["TimeSeries", "TimeSeriesList"]
