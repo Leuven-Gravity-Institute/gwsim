bcdf1021a4f6dc72edb3b7c18c6f1e96a8eafa49
Isaac C. F. Wong <chunfung.wong@kuleuven.be>
2025-12-10 13:58:49 +0100

    chore: Remove PyCBCStationaryGaussianNoiseSimulator

--- src/gwsim/noise/__init__.py
+++ src/gwsim/noise/__init__.py
@@ -8,12 +8,10 @@ from gwsim.noise.base import NoiseSimulator
 from gwsim.noise.bilby_stationary_gaussian import BilbyStationaryGaussianNoiseSimulator
 from gwsim.noise.colored_noise import ColoredNoiseSimulator
 from gwsim.noise.correlated_noise import CorrelatedNoiseSimulator
-from gwsim.noise.pycbc_stationary_gaussian import PyCBCStationaryGaussianNoiseSimulator
 
 __all__ = [
     "BilbyStationaryGaussianNoiseSimulator",
     "ColoredNoiseSimulator",
     "CorrelatedNoiseSimulator",
     "NoiseSimulator",
-    "PyCBCStationaryGaussianNoiseSimulator",
 ]
