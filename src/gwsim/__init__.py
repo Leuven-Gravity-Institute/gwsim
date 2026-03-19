8e28a812226aa1eb115eddaaea78c009112b36f8
Isaac C. F. Wong <23655285+isaac-cf-wong@users.noreply.github.com>
2026-03-19 05:28:38 +0000

    chore: Add `gwmock` to the dependencies and raise warnings when importing gwsim

--- src/gwsim/__init__.py
+++ src/gwsim/__init__.py
@@ -2,10 +2,25 @@
 
 from __future__ import annotations
 
+import warnings
+
 from . import utils
 from .utils.log import setup_logger
 from .version import __version__
 
 setup_logger()
+
+
+warnings.warn(
+    "This package `gwsim` has been renamed to `gwmock`. Please update your requirements and imports.",
+    DeprecationWarning,
+    stacklevel=2,
+)
+
+try:
+    from gwmock import *  # noqa: F403
+except ImportError as e:
+    # This handles the edge case where the dependency installation failed
+    raise ImportError("gwsim requires gwmock to be installed. Please run 'pip install gwmock'.") from e
 
 __all__ = ["__version__", "utils"]
